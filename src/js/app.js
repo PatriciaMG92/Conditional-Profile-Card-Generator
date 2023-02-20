bc7e5f51af96ed1ffa5e96529ada5f770f101a4c
PatriciaMG92 <xiqitinah_92@hotmail.com>
2023-02-20 11:04:06 +0000

    links functionality and remove comments

--- src/js/app.js
+++ src/js/app.js
@@ -23,9 +23,8 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
@@ -45,16 +44,16 @@ function render(variables = {}) {
   if (variables.socialMediaPosition == "position-left")
     mediasPosition = `class="position-left"`;
 
-  let twitterName = "https://twitter.com/4geeksacademy";
+  let twitterName = "4geeksacademy";
   if (variables.twitter !== null) twitterName = `${variables.twitter}`;
 
-  let githubName = "https://github.com/4geeksacademy";
+  let githubName = "4geeksacademy";
   if (variables.github !== null) githubName = `${variables.github}`;
 
-  let linkedinName = "https://linkedin.com/4geeksacademy";
+  let linkedinName = "4geeksacademy";
   if (variables.linkedin !== null) linkedinName = `${variables.linkedin}`;
 
-  let instagramName = "https://instagram.com/4geeksacademy";
+  let instagramName = "4geeksacademy";
   if (variables.instagram !== null) instagramName = `${variables.instagram}`;
 
   let chosenCity = "Miami";
@@ -89,7 +88,6 @@ function render(variables = {}) {
       break;
   }
 
-  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
@@ -97,10 +95,10 @@ function render(variables = {}) {
           <h2>${job}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul ${mediasPosition}>
-            <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -111,15 +109,14 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the url of the image that will used as background for the profile cover
+
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
+
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
+
     socialMediaPosition: "position-left",
-    // social media usernames
+
     twitter: null,
     github: "alesanchezr",
     linkedin: null,
@@ -130,12 +127,11 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); //render the card for the first time
+  render(window.variables);
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      const attribute = e.target.getAttribute("for");
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -145,7 +141,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new valus
+      render(Object.assign(window.variables, values));
     });
   });
 };
